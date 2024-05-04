aa2abe14618a0706cb5e17af641d05957972419c
kolawole tamilore <63677298+TamynatorSama@users.noreply.github.com>
2024-05-04 09:42:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const {Server} = require("socket.io")
 const io = new Server(3000, { /* options */ });
 
 io.on("connection", (socket) => {
-    // console.log(socket)
     socket.on('msg',(data)=>{
         console.log("pod cast admin has joined the party")
     })
